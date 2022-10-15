cd6e90572af49ddf42395dfb5ccff73b00fa2603
allicpp <58084635+alli-cpp@users.noreply.github.com>
2022-10-16 00:27:24 +0300

    task fixes

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+initialize the models package
+"""
+
 from os import getenv
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
